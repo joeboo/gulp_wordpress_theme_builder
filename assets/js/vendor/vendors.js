079f8ebe3c1abd10d76271905f7104b5b8e78735
Joe Boo <kanisornjoe@gmail.com>
2016-08-26 14:52:31 +0000

    New assets

---
+++ assets/js/vendor/vendors.js
@@ -0,0 +1,4 @@
+var $ 	   				= require('jquery');
+var navigation 			= require('./navigation');
+var skipLinkFocusFix 	= require('./skip-link-focus-fix');
+var customizer 			= require('./customizer');
